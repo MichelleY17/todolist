532801ae4fbdbdf7fe3d0c8204f4270898499eb6
MichelleY17 <yanezdayanna47@gmail.com>
2024-03-26 12:05:40 +0100

    seocnd

--- main.js
+++ main.js
@@ -1,7 +1,41 @@
 import { todos } from './data/todos.js';
-
+    const todoList = document.querySelector('.list-group');
+    const showCompleted = document.getElementById('show-completed').checked;
 render();
 
 function render() {
-  console.log({todos})
-}
+    const toCompleteCount = todos.filter(todo => !todo.done).length;
+    const totalTaskCount = todos.length;
+    for (const todo of todos){
+        const task = document.createElement("li");
+        task.className = "list-group-item";
+        // console.log(task);
+        task.textContent = todo.description;
+        todoList.appendChild(task);
+    } 
+};
+
+
+// function addTodo(description) {
+    
+//     if (!description.trim()) {
+//         alert('TODO vuota');
+//         return;
+//     }
+//     if (todos.some(todo => todo.description === description)) {
+//         alert('TODO già esistente');
+//         return;
+//     }
+
+//     todos.push({ description, done: false });
+//     render();
+// }
+// document.getElementById('add-new-btn').addEventListener('click', () => {
+//     const input = document.querySelector('.input-group input');
+//     addTodo(input.value);
+//     input.value = '';
+// });
+
+// document.querySelector('.list-group').addEventListener('change', event => {
+//     if (event.target.matches('.form-check-input'));
+// })
